0c4b225a21323f2a305824299ca5d4f4bddaf2fa
yashkumar0707 <yashkumar0707@gmail.com>
2020-12-04 00:34:51 +0530

    Rating images added

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
+import { globalStyles, Images } from '../styles/global';
 import Card from '../shared/card'
 export default function ReviewDetails({ navigation }) {
     const pressHandler = () => {
         navigation.goBack()
     }
+    const rating = navigation.getParam('rating')
     return (
         <View style={globalStyles.container}>
             <Card>
                 <Text>{navigation.getParam('title')}</Text>
-                <Text>{navigation.getParam('rating')}</Text>
+                {/* <Text>{navigation.getParam('rating')}</Text> */}
                 <Text>{navigation.getParam('body')}</Text>
+                <View style={styles.rating}>
+                    <Text>Gamezone Rating:</Text>
+                    <Image source={Images.ratings[rating]}></Image>
+                </View>
                 {/* <Button title="Back to Home" onPress={pressHandler}></Button>*/}
             </Card>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee',
+    }
+})
